5216c69f349bb627844c4bb697d5cae361f36285
Anton Kovalyov <akovalyov@microsoft.com>
2022-07-20 15:38:04 -0700

    fix: Do not delete manifest folder if env variable is not set.

---
+++ src/Microsoft.Sbom.Api/Exceptions/ManifestFolderExistsException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Microsoft.Sbom.Api.Exceptions
+{
+    /// <summary>
+    /// Thrown when manifest folder already exists in output path.
+    /// </summary>
+    public class ManifestFolderExistsException : Exception
+    {
+        public ManifestFolderExistsException()
+        {
+        }
+
+        public ManifestFolderExistsException(string message)
+            : base(message)
+        {
+        }
+
+        public ManifestFolderExistsException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
