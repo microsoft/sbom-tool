ad5feca11da643280431c5bbe421dd84df4f68c0
Aasim Malladi <aamallad@microsoft.com>
2022-09-08 19:43:13 -0700

    Added copyright headers

--- src/Microsoft.Sbom.Parsers.Spdx22SbomParser/Parser/SbomFileParser.cs
+++ src/Microsoft.Sbom.Parsers.Spdx22SbomParser/Parser/SbomFileParser.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Sbom.Contracts;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Sbom.Contracts;
 using Microsoft.Sbom.Contracts.Enums;
 using Microsoft.Sbom.Exceptions;
 using System;
