525ea6acd2310bb5cafe0148768368056d1474bb
Kevin Sigmund <ksigmund@microsoft.com>
2022-12-02 14:51:52 -0800

    Add DockerReference Spdx, and Vcpkg component types

---
+++ src/Microsoft.Sbom.Adapters/Adapters/ComponentDetection/DockerReferenceComponentExtensions.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.ComponentDetection.Contracts.TypedComponent;
+using Microsoft.Sbom.Contracts;
+using Microsoft.Sbom.Contracts.Enums;
+using System.Collections.Generic;
+
+namespace Microsoft.Sbom.Adapters.ComponentDetection
+{
+    /// <summary>
+    /// Extensions methods for <see cref="DockerReferenceComponent"/>.
+    /// </summary>
+    internal static class DockerReferenceComponentExtensions
+    {
+        /// <summary>
+        /// Converts a <see cref="DockerReferenceComponent"/> to an <see cref="SBOMPackage"/>.
+        /// </summary>
+        public static SBOMPackage? ToSbomPackage(this DockerReferenceComponent dockerReferenceComponent) => new ()
+        {
+            Id = dockerReferenceComponent.Id,
+            PackageUrl = dockerReferenceComponent.PackageUrl?.ToString(),
+            PackageName = dockerReferenceComponent.Digest,
+            Checksum = new[]
+            {
+                new Checksum
+                {
+                    Algorithm = AlgorithmName.SHA256,
+                    ChecksumValue = dockerReferenceComponent.Digest
+                },
+            },
+            FilesAnalyzed = false,
+            Type = "docker"
+        };
+    }
+}
---
+++ src/Microsoft.Sbom.Adapters/Adapters/ComponentDetection/VcpkgComponentExtensions.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.ComponentDetection.Contracts.TypedComponent;
+using Microsoft.Sbom.Contracts;
+
+namespace Microsoft.Sbom.Adapters.ComponentDetection
+{
+    /// <summary>
+    /// Extensions methods for <see cref="VcpkgComponent"/>.
+    /// </summary>
+    internal static class VcpkgComponentExtensions
+    {
+        /// <summary>
+        /// Converts a <see cref="VcpkgComponent"/> to an <see cref="SBOMPackage"/>.
+        /// </summary>
+        public static SBOMPackage? ToSbomPackage(this VcpkgComponent vcpkgComponent) => new ()
+        {
+            Id = vcpkgComponent.Id,
+            PackageUrl = vcpkgComponent.PackageUrl?.ToString(),
+            PackageName = vcpkgComponent.Name,
+            PackageSource = vcpkgComponent.DownloadLocation,
+            PackageVersion = vcpkgComponent.Version,
+            FilesAnalyzed = false,
+            Type = "vcpkg"
+        };
+    }
+}
