fa648928589b57728e9feee8545e245490d8cdf2
Sebastian Gomez <segomez@microsoft.com>
2023-05-23 12:50:54 -0400

    Add Path Attribute.

---
+++ src/Microsoft.Sbom.Common/Config/Attributes/PathAttribute.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Microsoft.Sbom.Common.Config.Attributes;
+
+/// <summary>
+/// This attribute is used to mark a property as a path.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+public sealed class PathAttribute : Attribute
+{
+}
