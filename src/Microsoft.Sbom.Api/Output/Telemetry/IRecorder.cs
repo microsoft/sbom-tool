aafcd9a1069f9b157aa7f662e0d50e91d508c146
Sebastian Gomez <segomez@microsoft.com>
2023-05-26 10:11:32 -0400

    Unused import

--- src/Microsoft.Sbom.Api/Output/Telemetry/IRecorder.cs
+++ src/Microsoft.Sbom.Api/Output/Telemetry/IRecorder.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.ComponentDetection.Contracts.BcdeModels;
 using Microsoft.Sbom.Api.Entities;
 using Microsoft.Sbom.Extensions.Entities;
 
