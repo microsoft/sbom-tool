581db91e3b8fe7b143bd01ca8749f510c2380b70
Aasim Malladi <aamallad@microsoft.com>
2023-06-09 13:19:37 -0700

    working ids

--- spdx-3.0/Microsoft.Sbom.JsonSerializer/Converters/IdentifierConverter.cs
+++ spdx-3.0/Microsoft.Sbom.JsonSerializer/Converters/IdentifierConverter.cs
@@ -3,38 +3,22 @@ using System.Text.Json.Serialization;
 using Microsoft.Sbom.Spdx3_0.Core;
 
 namespace Microsoft.Sbom.Converters;
-internal class IdentifierConverter : JsonConverter<List<Element>>
+internal class IdentifierConverter : JsonConverter<Element>
 {
-    public override List<Element>? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    public override Element? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         throw new NotImplementedException();
     }
 
-    public override void Write(Utf8JsonWriter writer, List<Element> value, JsonSerializerOptions options)
-    {     
-        writer.WriteStartArray();
-
-        foreach (var item in value)
+    public override void Write(Utf8JsonWriter writer, Element value, JsonSerializerOptions options)
+    {
+        if (value is Identifier)
         {
-            writer.WriteStringValue(item?.spdxId?.AbsoluteUri);
+            writer.WriteStringValue(value.spdxId?.ToString());
         }
-
-        writer.WriteEndArray();
-        //if (value.Any(element => element is Identifier))
-        //{
-        //    writer.WriteStartArray();
-
-        //    foreach (var item in value)
-        //    {
-        //        writer.WriteStringValue(item?.spdxId?.AbsoluteUri);
-        //    }
-
-        //    writer.WriteEndArray();
-        //}
-        //else
-        //{
-        //    JsonSerializer.Serialize(writer, value, options);
-        //}
-
+        else
+        {
+            System.Text.Json.JsonSerializer.Serialize(writer, value, options);
+        }
     }
 }
