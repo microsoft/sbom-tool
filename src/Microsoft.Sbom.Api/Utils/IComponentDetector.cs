f5346a4acdba6ee7b85e665deed3a4998082d719
Sebastian Gomez <segomez@microsoft.com>
2023-07-24 16:41:37 -0400

    Add Copyright header.

--- src/Microsoft.Sbom.Api/Utils/IComponentDetector.cs
+++ src/Microsoft.Sbom.Api/Utils/IComponentDetector.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Threading.Tasks;
 using Microsoft.ComponentDetection.Contracts.BcdeModels;
 
 namespace Microsoft.Sbom.Api.Utils;
