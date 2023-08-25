3f9ff8c5bddc3c0eae596f485812baef177d54e9
Ryan Brandenburg <rybrande@microsoft.com>
2023-08-25 14:53:07 -0700

    Handle BOM's at the begining of SbomParser streams

--- src/Microsoft.Sbom.Parsers.Spdx22SbomParser/SPDXParser.cs
+++ src/Microsoft.Sbom.Parsers.Spdx22SbomParser/SPDXParser.cs
@@ -95,10 +95,56 @@ public class SPDXParser : ISbomParser
             throw new ArgumentException($"The {nameof(buffer)} value can't be null or of 0 length.");
         }
 
+        // Utf8JsonReader will not handle BOM's, so we need to "eat" them before.
+        this.stream.Position = GetCursor(this.stream);
+
         // Fill up the buffer.
         if (!stream.CanRead || stream.Read(buffer) == 0)
         {
             throw new EndOfStreamException();
+        }
+
+        static int GetCursor(Stream stream)
+        {
+            // UTF-32, big-endian
+            if (IsMatch(stream, new byte[] { 0x00, 0x00, 0xFE, 0xFF }))
+            {
+                return 4;
+            }
+
+            // UTF-32, little-endian
+            if (IsMatch(stream, new byte[] { 0xFF, 0xFE, 0x00, 0x00 }))
+            {
+                return 4;
+            }
+
+            // UTF-16, big-endian
+            if (IsMatch(stream, new byte[] { 0xFE, 0xFF }))
+            {
+                return 2;
+            }
+
+            // UTF-16, little-endian
+            if (IsMatch(stream, new byte[] { 0xFF, 0xFE }))
+            {
+                return 2;
+            }
+
+            // UTF-8
+            if (IsMatch(stream, new byte[] { 0xEF, 0xBB, 0xBF }))
+            {
+                return 3;
+            }
+
+            return 0;
+        }
+
+        static bool IsMatch(Stream stream, byte[] match)
+        {
+            stream.Position = 0;
+            var buffer = new byte[match.Length];
+            stream.Read(buffer, 0, buffer.Length);
+            return !buffer.Where((t, i) => t != match[i]).Any();
         }
     }
 
