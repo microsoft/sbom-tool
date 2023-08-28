27ce43425da5ccec249385830da7fa557271b209
Sebastian Gomez <segomez@microsoft.com>
2023-08-28 13:57:28 -0400

    Add header and fix warning.

--- src/Microsoft.Sbom.Adapters/Adapters/ComponentDetection/ScanResultWithLicense.cs
+++ src/Microsoft.Sbom.Adapters/Adapters/ComponentDetection/ScanResultWithLicense.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
 using Microsoft.ComponentDetection.Contracts.BcdeModels;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
@@ -8,5 +11,5 @@ namespace Microsoft.Sbom.Adapters.ComponentDetection;
 [JsonObject(MemberSerialization.OptOut, NamingStrategyType = typeof(CamelCaseNamingStrategy))]
 public sealed class ScanResultWithLicense : ScanResult
 {
-    public new IEnumerable<ScannedComponentWithLicense> ComponentsFound { get; init; }
+    public new IEnumerable<ScannedComponentWithLicense>? ComponentsFound { get; init; }
 }
