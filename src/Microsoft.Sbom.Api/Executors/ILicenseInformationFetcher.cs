958d2769a42717837313f84691109e6dfd056052
Sebastian Gomez <segomez@microsoft.com>
2023-09-05 17:04:50 -0400

    Styling Error fix

--- src/Microsoft.Sbom.Api/Executors/ILicenseInformationFetcher.cs
+++ src/Microsoft.Sbom.Api/Executors/ILicenseInformationFetcher.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.ComponentDetection.Contracts.BcdeModels;
 
 namespace Microsoft.Sbom.Api.Executors;
+
 public interface ILicenseInformationFetcher
 {
     /// <summary>
