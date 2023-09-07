f5d23a2be6b8aa4273c9661a285ab02fbd01e57a
sebasgomez238 <69322674+sebasgomez238@users.noreply.github.com>
2023-09-07 12:01:03 -0400

    Add license retrieval for Cargo, Pip, Ruby, Cocoapods. (#369)
    Co-authored-by: Sebastian Gomez <segomez@microsoft.com>

--- src/Microsoft.Sbom.Adapters/Adapters/ComponentDetection/RubyGemsComponentExtensions.cs
+++ src/Microsoft.Sbom.Adapters/Adapters/ComponentDetection/RubyGemsComponentExtensions.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Microsoft.ComponentDetection.Contracts.TypedComponent;
@@ -14,13 +14,14 @@ internal static class RubyGemsComponentExtensions
     /// <summary>
     /// Converts a <see cref="RubyGemsComponent"/> to an <see cref="SbomPackage"/>.
     /// </summary>
-    public static SbomPackage? ToSbomPackage(this RubyGemsComponent rubyGemsComponent) => new()
+    public static SbomPackage? ToSbomPackage(this RubyGemsComponent rubyGemsComponent, string? license = null) => new()
     {
         Id = rubyGemsComponent.Id,
         PackageUrl = rubyGemsComponent.PackageUrl?.ToString(),
         PackageName = rubyGemsComponent.Name,
         PackageVersion = rubyGemsComponent.Version,
         PackageSource = rubyGemsComponent.Source,
+        LicenseInfo = string.IsNullOrWhiteSpace(license) ? null : new LicenseInfo { Concluded = license },
         FilesAnalyzed = false,
         Type = "ruby"
     };
