43a1bb62799f1c7a46680c549f01315fe8094262
Ryan Brandenburg <rybrande@microsoft.com>
2023-09-22 14:20:33 -0700

    Code coverage

---
+++ test/Microsoft.Sbom.Parsers.Spdx22SbomParser.Tests/Utils/SbomFormatConverterTests.cs
@@ -0,0 +1,52 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using JsonAsynchronousNodeKit.Exceptions;
+using Microsoft.Sbom.Parsers.Spdx22SbomParser.Entities;
+using Microsoft.Sbom.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Sbom.Parsers.Spdx22SbomParser.Utils.Tests;
+
+[TestClass]
+public class SbomFormatConverterTests
+{
+    [TestMethod]
+    public void ToSbomPackage_HappyPath()
+    {
+        var spdxPackage = new SPDXPackage
+        {
+            Name = "name",
+            SpdxId = "spdxId",
+            LicenseConcluded = "licenseConcluded",
+            LicenseDeclared = "licenseDeclared",
+            CopyrightText = "copyright",
+            Supplier = "supplier",
+        };
+
+        var sbomPackage = spdxPackage.ToSbomPackage();
+
+        Assert.IsNotNull(sbomPackage);
+        Assert.AreEqual(spdxPackage.Name, sbomPackage.PackageName);
+        Assert.AreEqual(spdxPackage.SpdxId, sbomPackage.Id);
+    }
+
+    [TestMethod]
+    public void ToSbomPackage_FailsOnEmptyLicenseInfo()
+    {
+        var spdxPackage = new SPDXPackage
+        {
+            Name = "name",
+            SpdxId = "spdxId",
+            LicenseConcluded = "licenseConcluded",
+            LicenseDeclared = "licenseDeclared",
+            CopyrightText = "copyright",
+            Supplier = "supplier",
+            FilesAnalyzed = true,
+            LicenseInfoFromFiles = new List<string>(),
+        };
+
+        Assert.ThrowsException<ParserException>(spdxPackage.ToSbomPackage);
+    }
+}
