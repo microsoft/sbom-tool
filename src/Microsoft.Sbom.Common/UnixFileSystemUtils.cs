ed980a735fec8c88ff0c5bff0c43a80c87870088
Sebastian Gomez <segomez@microsoft.com>
2023-10-11 21:32:42 -0400

    Fix missing mono.posix on ARM devices.

--- src/Microsoft.Sbom.Common/UnixFileSystemUtils.cs
+++ src/Microsoft.Sbom.Common/UnixFileSystemUtils.cs
@@ -1,10 +1,14 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace Microsoft.Sbom.Common;
 
+using System.IO;
+
+#if NET6_0
 using Mono.Unix;
 using Mono.Unix.Native;
+#endif
 
 /// <summary>
 /// Wrapper around file system functions. Used for unit testing.
@@ -12,6 +16,7 @@ using Mono.Unix.Native;
 /// </summary>
 internal class UnixFileSystemUtils : FileSystemUtils
 {
+#if NET6_0
     /// <inheritdoc />
     public override bool DirectoryHasReadPermissions(string directoryPath)
     {
@@ -25,4 +30,20 @@ internal class UnixFileSystemUtils : FileSystemUtils
         var directoryInfo = new UnixDirectoryInfo(directoryPath);
         return directoryInfo.CanAccess(AccessModes.W_OK) && directoryInfo.CanAccess(AccessModes.F_OK);
     }
+#endif
+
+#if NET8_0_OR_GREATER
+    public override bool DirectoryHasReadPermissions(string directoryPath)
+    {
+        var fileMode = File.GetUnixFileMode(directoryPath);
+        return fileMode == (UnixFileMode.GroupRead | UnixFileMode.OtherRead | UnixFileMode.UserRead);
+    }
+
+    public override bool DirectoryHasWritePermissions(string directoryPath)
+    {
+        var fileMode = File.GetUnixFileMode(directoryPath);
+        return fileMode == (UnixFileMode.GroupWrite | UnixFileMode.OtherWrite | UnixFileMode.UserWrite);
+    }
+#endif
+
 }
