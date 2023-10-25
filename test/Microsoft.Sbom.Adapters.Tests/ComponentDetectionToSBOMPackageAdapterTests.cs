fe01100568e4052f60a2f741728dd913f590b956
Sebastian Gomez <segomez@microsoft.com>
2023-10-25 09:40:10 -0400

    Add Conan support.

--- test/Microsoft.Sbom.Adapters.Tests/ComponentDetectionToSBOMPackageAdapterTests.cs
+++ test/Microsoft.Sbom.Adapters.Tests/ComponentDetectionToSBOMPackageAdapterTests.cs
@@ -129,6 +129,26 @@ public class ComponentDetectionToSBOMPackageAdapterTests
     }
 
     [TestMethod]
+    public void ConanComponent_ToSbomPackage()
+    {
+        var md5 = Guid.NewGuid().ToString();
+        var sha1Hash = Guid.NewGuid().ToString();
+
+        var conanComponent = new ConanComponent("name", "version", md5, sha1Hash);
+        var scannedComponent = new ScannedComponentWithLicense() { Component = conanComponent };
+
+        var sbomPackage = scannedComponent.ToSbomPackage(new AdapterReport());
+
+        Assert.IsNotNull(sbomPackage.Id);
+        Assert.IsNotNull(sbomPackage.PackageUrl);
+        Assert.AreEqual(conanComponent.Name, sbomPackage.PackageName);
+        Assert.AreEqual(conanComponent.Version, sbomPackage.PackageVersion);
+        Assert.IsNotNull(sbomPackage.Checksum.First(x => x.ChecksumValue == conanComponent.Md5Hash));
+        Assert.IsNotNull(sbomPackage.Checksum.First(x => x.ChecksumValue == conanComponent.Sha1Hash));
+        Assert.AreEqual(conanComponent.PackageSourceURL, sbomPackage.PackageSource);
+    }
+
+    [TestMethod]
     public void CondaComponent_ToSbomPackage()
     {
         var condaComponent = new CondaComponent("name", "version", "build", "channel", "subdir", "namespace", "http://microsoft.com", "md5");
