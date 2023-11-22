fd8ead20f917fe8d9358b024d4bb5690f232ad27
Sebastian Gomez <segomez@microsoft.com>
2023-11-22 11:20:21 -0500

    Remove unused imports.

--- test/Microsoft.Sbom.Api.Tests/PackageDetails/RubyGemsUtilsTests.cs
+++ test/Microsoft.Sbom.Api.Tests/PackageDetails/RubyGemsUtilsTests.cs
@@ -1,12 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-using System.IO;
-using System.Runtime.InteropServices;
-using System.Text;
-using Microsoft.ComponentDetection.Contracts.BcdeModels;
-using Microsoft.ComponentDetection.Contracts.TypedComponent;
 using Microsoft.Sbom.Api.Output.Telemetry;
 using Microsoft.Sbom.Api.PackageDetails;
 using Microsoft.Sbom.Common;
