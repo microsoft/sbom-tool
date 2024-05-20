743f13e35c3b735826dad5033c9ff49161d4b71e
Sarah Oslund <sfoslund@microsoft.com>
2024-05-20 10:44:27 -0700

    Add redact documentation

--- src/Microsoft.Sbom.Api/Config/SbomToolCmdRunner.cs
+++ src/Microsoft.Sbom.Api/Config/SbomToolCmdRunner.cs
@@ -59,7 +59,6 @@ public class SbomToolCmdRunner
     /// </summary>
     [ArgActionMethod]
     [ArgDescription("Redact file information from given SBOM(s).")]
-    [OmitFromUsageDocs]
     public RedactArgs Redact(RedactArgs redactArgs)
     {
         return redactArgs;
