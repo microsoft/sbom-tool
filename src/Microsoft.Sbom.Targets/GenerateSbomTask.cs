6cce492141c2bced6980ff998838096f0bd31759
vpatakottu <vpatakottu@microsoft.com>
2024-05-24 12:51:20 -0700

    Add all arguments to GenerateSBOMTask

--- src/Microsoft.Sbom.Targets/GenerateSbomTask.cs
+++ src/Microsoft.Sbom.Targets/GenerateSbomTask.cs
@@ -17,20 +17,85 @@ public class GenerateSbomTask : Task
 {
     // TODO it is possible we will want to expose additional arguments, either as required or optional.
     // Will need to get SDK team/ windows team input on which arguments are necessary.
+
+    /// <summary>
+    /// The path to the drop directory for which the SBOM will be generated
+    /// </summary>
     [Required]
     public string BuildDropPath { get; set; }
 
+    /// <summary>
+    /// The path to the directory containing build components and package information.
+    /// For example, path to a .csproj or packages.config file.
+    /// </summary>
     [Required]
     public string BuildComponentPath { get; set; }
 
+    /// <summary>
+    /// Supplier of the package the SBOM represents.
+    /// </summary>
     [Required]
     public string PackageSupplier { get; set; }
 
+    /// <summary>
+    /// Name of the package the SBOM represents.
+    /// </summary>
     [Required]
     public string PackageName { get; set; }
 
+    /// <summary>
+    /// Version of the package the SBOM represents.
+    /// </summary>
     [Required]
     public string PackageVersion { get; set; }
+
+    /// <summary>
+    /// The base path of the SBOM namespace uri.
+    /// </summary>
+    [Required]
+    public string NamespaceBaseUri { get; set; }
+
+    /// <summary>
+    /// A unique URI part that will be appended to NamespaceBaseUri.
+    /// </summary>
+    public string NamespaceUriUniquePart { get; set; }
+
+    /// <summary>
+    /// The path to a file containing a list of external SBOMs that will be appended to the
+    /// SBOM that is being generated.
+    /// </summary>
+    public string ExternalDocumentListFile { get; set; }
+
+    /// <summary>
+    /// If true, it will fetch licensing information for detected packages.
+    /// </summary>
+    public bool FetchLicenseInformation { get; set; }
+
+    /// <summary>
+    /// If true, it will parse licensing and supplier information from a packages metadata file.
+    /// </summary>
+    public bool EnablePackageMetadataParsing { get; set; }
+
+    /// <summary>
+    /// Determines how detailed the outputed logging will be.
+    /// </summary>
+    public string Verbosity { get; set; }
+
+    /// <summary>
+    /// A list of the name and version of the manifest format being used.
+    /// </summary>
+    public string ManifestInfo { get; set; }
+
+    /// <summary>
+    /// If true, it will delete the previously generated SBOM manifest directory before
+    /// generating a new SBOM in ManifestDirPath.
+    /// </summary>
+    public bool DeleteManifestDirIfPresent { get; set; }
+
+    /// <summary>
+    /// The path where the SBOM will be generated.
+    /// </summary>
+    public string ManifestDirPath { get; set; }
 
     [Output]
     public string SbomPath { get; set; }
