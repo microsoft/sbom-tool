8ffff9cd2770a4ed1fe35b78cfeb0ec0f54cfe2d
Gustavo Castellanos Alfonzo <gustavoaca1997@gmail.com>
2024-05-27 17:54:47 -0700

    Add call to the SBOM API from GenerateSbomTask#Execute (#2)
    * Start implementing Execute
    
    * Make SBOM Targets test target .NET 8 only.
    
    * Make the Component Path not required.
    
    * Build the current project in test, and check that the manifest was generated.
    
    * Ad a few comments for TODOs.

--- src/Microsoft.Sbom.Targets/GenerateSbomTask.cs
+++ src/Microsoft.Sbom.Targets/GenerateSbomTask.cs
@@ -4,11 +4,15 @@
 namespace Microsoft.Sbom.Targets;
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Threading;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Sbom.Api;
+using Microsoft.Sbom.Contracts;
 using Microsoft.Sbom.Extensions.DependencyInjection;
 using Microsoft.Sbom.Tool;
 using PowerArgs;
@@ -23,13 +27,6 @@ public class GenerateSbomTask : Task
     /// </summary>
     [Required]
     public string BuildDropPath { get; set; }
-
-    /// <summary>
-    /// The path to the directory containing build components and package information.
-    /// For example, path to a .csproj or packages.config file.
-    /// </summary>
-    [Required]
-    public string BuildComponentPath { get; set; }
 
     /// <summary>
     /// Supplier of the package the SBOM represents.
@@ -54,6 +51,12 @@ public class GenerateSbomTask : Task
     /// </summary>
     [Required]
     public string NamespaceBaseUri { get; set; }
+
+        /// <summary>
+    /// The path to the directory containing build components and package information.
+    /// For example, path to a .csproj or packages.config file.
+    /// </summary>
+    public string BuildComponentPath { get; set; }
 
     /// <summary>
     /// A unique URI part that will be appended to NamespaceBaseUri.
@@ -90,7 +93,7 @@ public class GenerateSbomTask : Task
     /// If true, it will delete the previously generated SBOM manifest directory before
     /// generating a new SBOM in ManifestDirPath.
     /// </summary>
-    public bool DeleteManifestDirIfPresent { get; set; }
+    public bool DeleteManifestDirIfPresent { get; set; } = true;
 
     /// <summary>
     /// The path where the SBOM will be generated.
@@ -100,10 +103,22 @@ public class GenerateSbomTask : Task
     [Output]
     public string SbomPath { get; set; }
 
+    private ISBOMGenerator Generator { get; set; }
+
+    public GenerateSbomTask()
+    {
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureServices((host, services) =>
+                services
+                .AddSbomTool())
+            .Build();
+        this.Generator = host.Services.GetRequiredService<ISBOMGenerator>();
+    }
+
     public override bool Execute()
     {
+        // TODO: Validate all arguments.
         if (string.IsNullOrEmpty(BuildDropPath) ||
-            string.IsNullOrEmpty(BuildComponentPath) ||
             string.IsNullOrEmpty(PackageSupplier) ||
             string.IsNullOrEmpty(PackageName) ||
             string.IsNullOrEmpty(PackageVersion))
@@ -114,12 +129,36 @@ public class GenerateSbomTask : Task
 
         try
         {
-            // TODO replace this with a call to SBOM API to generate SBOM
+            var sbomMetadata = new SBOMMetadata
+            {
+                PackageSupplier = this.PackageSupplier,
+                PackageName = this.PackageName,
+                PackageVersion = this.PackageVersion,
+            };
+            var runtimeConfiguration = new RuntimeConfiguration
+            {
+                NamespaceUriBase = this.NamespaceBaseUri,
+                NamespaceUriUniquePart = this.NamespaceUriUniquePart,
+                DeleteManifestDirectoryIfPresent = this.DeleteManifestDirIfPresent,
+                Verbosity = Enum.TryParse(this.Verbosity, out EventLevel eventLevel) ? eventLevel : default, // TODO: validate this
+            };
+#pragma warning disable VSTHRD002 // Avoid problematic synchronous waits
+            var result = System.Threading.Tasks.Task.Run(() => this.Generator.GenerateSbomAsync(
+                rootPath: this.BuildDropPath,
+                manifestDirPath: this.ManifestDirPath,
+                metadata: sbomMetadata,
+                componentPath: this.BuildComponentPath,
+                runtimeConfiguration: runtimeConfiguration,
+                specifications: !string.IsNullOrWhiteSpace(this.ManifestInfo) ? [SbomSpecification.Parse(this.ManifestInfo)] : null,
+                externalDocumentReferenceListFile: this.ExternalDocumentListFile)).GetAwaiter().GetResult();
+#pragma warning restore VSTHRD002 // Avoid problematic synchronous waits
+
             SbomPath = "path/to/sbom";
-            return true;
+            return result.IsSuccessful;
         }
         catch (Exception e)
         {
+            // TODO: Add automated tests for the different exceptions.
             Log.LogError($"SBOM generation failed: {e.Message}");
             return false;
         }
