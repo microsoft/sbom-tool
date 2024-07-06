34273f49080f6bd746d34eface4860381b0e612c
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2024-07-05 20:16:55 -0700

    Remove unneeded using statements

--- test/Microsoft.Sbom.Tool.Tests/IntegrationTests.cs
+++ test/Microsoft.Sbom.Tool.Tests/IntegrationTests.cs
@@ -2,14 +2,11 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NuGet.Frameworks;
 
 namespace Microsoft.Sbom.Tools.Tests;
 
