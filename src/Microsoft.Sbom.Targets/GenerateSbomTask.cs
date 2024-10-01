7b90dd481a842936928242f367c4152efa252aaa
Chet Husk <chusk3@gmail.com>
2024-09-30 21:25:15 -0500

    Sidestep a timing issue with the TaskLoggingHelper in the GenerateSbomTask's Logging infrastructure
    Also generate binlogs for the tests to allow precise investigation of the builds

--- src/Microsoft.Sbom.Targets/GenerateSbomTask.cs
+++ src/Microsoft.Sbom.Targets/GenerateSbomTask.cs
@@ -28,7 +28,7 @@ using SPDX30 = Microsoft.Sbom.Parsers.Spdx30SbomParser;
 /// </summary>
 public partial class GenerateSbom : Task
 {
-    private ISbomGenerator Generator { get; set; }
+    private readonly IHost taskHost;
 
     /// <summary>
     /// Constructor for the GenerateSbomTask.
@@ -36,7 +36,7 @@ public partial class GenerateSbom : Task
     public GenerateSbom()
     {
         var taskLoggingHelper = new TaskLoggingHelper(this);
-        var host = Host.CreateDefaultBuilder()
+        taskHost = Host.CreateDefaultBuilder()
             .ConfigureServices((host, services) =>
                 services
                 .AddSbomTool(LogEventLevel.Information, taskLoggingHelper)
@@ -62,7 +62,6 @@ public partial class GenerateSbom : Task
                 .AddSingleton<IManifestConfigHandler, SPDX22ManifestConfigHandler>()
                 .AddSingleton<IManifestConfigHandler, SPDX30ManifestConfigHandler>())
             .Build();
-        this.Generator = host.Services.GetRequiredService<ISbomGenerator>();
     }
 
     /// <inheritdoc/>
@@ -76,7 +75,9 @@ public partial class GenerateSbom : Task
                 return false;
             }
 
-            // Set other configurations. The GenerateSbomAsync() already sanitizes and checks for
+            var generator = taskHost.Services.GetRequiredService<ISbomGenerator>();
+
+            // Set other configurations. The GenerateSBOMAsync() already sanitizes and checks for
             // a valid namespace URI and generates a random guid for NamespaceUriUniquePart if
             // one is not provided.
             var sbomMetadata = new SbomMetadata
@@ -93,7 +94,7 @@ public partial class GenerateSbom : Task
                 Verbosity = ValidateAndAssignVerbosity(),
             };
 #pragma warning disable VSTHRD002 // Avoid problematic synchronous waits
-            var result = System.Threading.Tasks.Task.Run(() => this.Generator.GenerateSbomAsync(
+            var result = System.Threading.Tasks.Task.Run(() => generator.GenerateSbomAsync(
                 rootPath: this.BuildDropPath,
                 manifestDirPath: this.ManifestDirPath,
                 metadata: sbomMetadata,
