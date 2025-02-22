19454be5735ae78b54df10ac9c69846cdc6f1fa2
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2025-02-21 22:29:32 -0800

    SBOMPackagesProvider_ => SbomPackagesProvider

--- src/Microsoft.Sbom.Targets/GenerateSbomTask.cs
+++ src/Microsoft.Sbom.Targets/GenerateSbomTask.cs
@@ -41,7 +41,7 @@ public partial class GenerateSbom : Task
                 /* Manually adding some dependencies since `AddSbomTool()` does not add them when
                  * running the MSBuild Task from another project.
                  */
-                .AddSingleton<ISourcesProvider, SBOMPackagesProvider_>()
+                .AddSingleton<ISourcesProvider, SbomPackagesProvider>()
                 .AddSingleton<ISourcesProvider, CGExternalDocumentReferenceProvider>()
                 .AddSingleton<ISourcesProvider, DirectoryTraversingFileToJsonProvider>()
                 .AddSingleton<ISourcesProvider, ExternalDocumentReferenceFileProvider>()
