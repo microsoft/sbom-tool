34835e9c1f73995534597e76edb9874609e799d9
ppandrate <ppandrate@microsoft.com>
2025-03-12 14:36:57 -0700

    remove unused directives

--- src/Microsoft.Sbom.Tool/ValidationService.cs
+++ src/Microsoft.Sbom.Tool/ValidationService.cs
@@ -2,13 +2,11 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Sbom.Api;
 using Microsoft.Sbom.Api.Output.Telemetry;
-using Microsoft.Sbom.Api.Utils;
 using Microsoft.Sbom.Api.Workflows;
 
 namespace Microsoft.Sbom.Tool;
