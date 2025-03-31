bb4a9e59bcb5e672bb3fdbbfb20ff7f7a9005029
ppandrate <ppandrate@microsoft.com>
2025-03-31 12:50:29 -0700

    grammar

--- src/Microsoft.Sbom.Api/Config/ConfigSanitizer.cs
+++ src/Microsoft.Sbom.Api/Config/ConfigSanitizer.cs
@@ -237,7 +237,7 @@ public class ConfigSanitizer
         // Compliance standard is only supported for ManifestInfo value of SPDX 3.0 and above.
         if (!newValue.Equals(ComplianceStandardType.None) && !configuration.ManifestInfo.Value.Any(mi => mi.Equals(Constants.SPDX30ManifestInfo)))
         {
-            throw new ValidationArgException($"Compliance standard {newValue.Name} is not supported with ManifestInfo value of {configuration.ManifestInfo.Value.First()}. " +
+            throw new ValidationArgException($"Compliance standard {newValue.Name} is not supported with ManifestInfo value of {configuration.ManifestInfo.Value.First()}." +
                 $"Please use a supported combination.");
         }
         else
