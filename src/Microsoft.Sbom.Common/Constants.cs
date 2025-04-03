c8d38d1e3d0467da8796fd083aa83f1e0f598e87
ppandrate <ppandrate@microsoft.com>
2025-04-03 15:40:18 -0700

    fix formatting

--- src/Microsoft.Sbom.Common/Constants.cs
+++ src/Microsoft.Sbom.Common/Constants.cs
@@ -20,7 +20,7 @@ public static class Constants
 
     public const string DefaultManifestInfoName = "SPDX";
     public const string DefaultManifestInfoVersion = "2.2";
-  
+
     public const string SPDXContextHeaderName = "@context";
     public const string SPDXGraphHeaderName = "@graph";
     public const string SPDXRefFile = "SPDXRef-File";
