f8bd74a230c3a388004520b23c4f5c76140b312c
ppandrate <ppandrate@microsoft.com>
2025-04-21 16:18:17 -0700

    remove unnecessary changes

--- src/Microsoft.Sbom.Parsers.Spdx30SbomParser/Utils/SPDXToSbomFormatConverterExtensions.cs
+++ src/Microsoft.Sbom.Parsers.Spdx30SbomParser/Utils/SPDXToSbomFormatConverterExtensions.cs
@@ -185,30 +185,15 @@ public static class SPDXToSbomFormatConverterExtensions
 
     private static string GetSupplier(this Package spdxPackage, List<Element> spdx30Elements)
     {
-        var organizationSpdxId = spdxPackage.GetOrganizationSpdxId(spdx30Elements);
+        var organizationSpdxId = spdxPackage.SuppliedBy;
         if (organizationSpdxId is null)
         {
             return null;
         }
 
         var organizationElement = spdx30Elements
-            .FirstOrDefault(element => element.SpdxId == organizationSpdxId);
+            .FirstOrDefault(element => element is Organization && element.SpdxId == organizationSpdxId);
         return organizationElement?.Name;
-    }
-
-    private static string GetOrganizationSpdxId(this Package spdxPackage, List<Element> spdx30Elements)
-    {
-        // Handle special case for the root package.
-        if (spdxPackage.SpdxId == Parsers.Spdx30SbomParser.Constants.RootPackageIdValue)
-        {
-            var creationInfo = spdx30Elements
-            .First(element => element is CreationInfo) as CreationInfo;
-            return creationInfo.CreatedBy.First();
-        }
-        else
-        {
-            return spdxPackage.SuppliedBy;
-        }
     }
 
     private static string GetPackageUrl(this Package spdxPackage, List<Element> spdx30Elements)
