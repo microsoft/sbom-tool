708a2d6595314411066d219defff2d76f81231d7
Simon Cropp <simon.cropp@gmail.com>
2025-05-27 05:30:09 +1000

    remove unused Program fields (#1086)
    Co-authored-by: Dave Tryon <45672944+DaveTryon@users.noreply.github.com>

--- src/Microsoft.Sbom.Tool/Program.cs
+++ src/Microsoft.Sbom.Tool/Program.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -18,20 +17,6 @@ namespace Microsoft.Sbom.Tool;
 
 internal class Program
 {
-    internal static string Name => NameValue.Value;
-
-    internal static string Version => VersionValue.Value;
-
-    private static readonly Lazy<string> NameValue = new Lazy<string>(() =>
-    {
-        return typeof(Program).Assembly.GetCustomAttribute<AssemblyProductAttribute>()?.Product ?? "sbomtool";
-    });
-
-    private static readonly Lazy<string> VersionValue = new Lazy<string>(() =>
-    {
-        return typeof(Program).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? string.Empty;
-    });
-
     public static async Task Main(string[] args)
     {
         var result = await Args.InvokeActionAsync<SbomToolCmdRunner>(args);
