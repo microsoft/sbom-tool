df0ff35802e8793ce2d2cc7d2a257c6799c24050
Simon Cropp <simon.cropp@gmail.com>
2025-06-26 23:29:57 +1000

    Update SbomConfigProvider.cs

--- src/Microsoft.Sbom.Api/Manifest/Configuration/SbomConfigProvider.cs
+++ src/Microsoft.Sbom.Api/Manifest/Configuration/SbomConfigProvider.cs
@@ -149,7 +149,7 @@ public class SbomConfigProvider : ISbomConfigProvider
     {
         if (MetadataDictionary.TryGetValue(key, out var value))
         {
-            logger.Debug($"Found value for header {key} in internal metadata.");
+            logger.Debug("Found value for header {MetadataKey} in internal metadata.", key);
             return value;
         }
 
@@ -160,7 +160,7 @@ public class SbomConfigProvider : ISbomConfigProvider
     {
         if (MetadataDictionary.TryGetValue(key, out value))
         {
-            logger.Debug($"Found value for header {key} in internal metadata.");
+            logger.Debug("Found value for header {MetadataKey} in internal metadata.", key);
             return true;
         }
 
