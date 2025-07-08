5f1cda210501598f64340cef06867d7116883ac2
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2025-07-08 13:29:09 -0700

    namespace after using

--- src/Microsoft.Sbom.Contracts/Contracts/SbomAggregationResult.cs
+++ src/Microsoft.Sbom.Contracts/Contracts/SbomAggregationResult.cs
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Microsoft.Sbom.Contracts;
-
 using System.Collections.Generic;
+
+namespace Microsoft.Sbom.Contracts;
 
 /// <summary>
 /// Represents the result of a SBOM aggregation action.
